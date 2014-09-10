eb5b438647d2a9a67c3c9561517edc20772b776f
Cathleen Gendron <gendron@mit.edu>
2014-09-10 13:36:02 -0400

    added files for initial build

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			countText.text = "Count: " + count.ToString ();
+			if(count == 6)
+			{
+				winText.text = "You win!";
+			}
+		}
+	}
+
+}
